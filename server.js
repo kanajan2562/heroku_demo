7ec068847fee0f8488ee18c12ce56450da9ca9a6
tummanoon <kanajan2562@gmail.com>
2021-08-26 14:04:38 +0700

    ok

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5500;
 
 app.get("/",(req,res)=>{
-    res.json({result:"ok",data:[1,2,3,4,5]});
+    res.json({result:"ok",data:[1,2,3,4,5,6,7,8,9,10]});
 
 }); 
 
