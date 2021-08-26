44c343f7f7cd56ba45a20a146e7852ea60126e23
tummanoon <kanajan2562@gmail.com>
2021-08-26 13:26:48 +0700

    init

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5500;
 app.get("/",(req,res)=>{
     res.json({result:"ok",data:[1,2,3,4,5]});
 
-});
+}); 
 
 app.listen(PORT,()=>{
     console.log(`Server is running. ${PORT}`);
